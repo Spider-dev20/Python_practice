b0704338b5811cc116e6809578f4fa353aab3a8e
Spider-dev20 <67261837+Spider-dev20@users.noreply.github.com>
2020-07-26 16:11:29 +0200

    Add files via upload

--- task.py
+++ task.py
@@ -1,36 +1,17 @@
-class Game:
-    money = int(input())
+class PiggyBank:
 
-    def __init__(self, price, name, plural):
-        self.price = price
-        self.name = name
-        self.plural = plural
-        self.calculation = Game.money // price
+    def __init__(self, dollars, cents):
+        self.dollars = dollars
+        self.cents = cents
 
+    def add_money(self, deposit_dollars, deposit_cents):
+        self.dollars += deposit_dollars
+        self.dollars += (self.cents + deposit_cents) // 100
+        self.cents = (self.cents + deposit_cents) % 100
 
-    def animal(self):
-        if self.calculation == 1:
-            print(self.calculation, self.name)
-        else:
-            print(self.calculation, self.plural)
+        print(self.dollars, self.cents)
 
 
-chicken = Game(23, "chicken", "chickens")
-goat = Game(678, "goat", "goats")
-pig = Game(1296, "pig", "pigs")
-cow = Game(3848, "cow", "cows")
-sheep = Game(6769, "sheep", "sheep")
+transfer = PiggyBank(1, 2)
 
-
-if Game.money >= 6769:
-    sheep.animal()
-elif Game.money >= 3848:
-    cow.animal()
-elif Game.money >= 1296:
-    pig.animal()
-elif Game.money >= 678:
-    goat.animal()
-elif Game.money >= 23:
-    chicken.animal()
-else:
-    print("None")
+transfer.add_money(500, 98)
